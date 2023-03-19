f0b3479dc6767325c40ba940142368b4f9230389
Grunet <sadikshahidain@gmail.com>
2023-03-19 00:02:24 +0000

    return more of an HTML page

--- demo-with-custom-backend/src/index.js
+++ demo-with-custom-backend/src/index.js
@@ -10,6 +10,21 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
+		return new Response(`
+			<!DOCTYPE html>
+			<html lang="en">
+				<head>
+					<title>E2E Analytics for Accessibility Demo</title>
+					<meta name="description" content="An end-to-end demo of analytics for accessibility using a custom backend">
+					<meta charset="UTF-8">
+					<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				</head>
+				<body>
+					<main>
+						<h1>This needs better text</h1>
+					</main>
+				</body>
+			</html>
+		`);
 	},
 };
